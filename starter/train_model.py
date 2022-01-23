6908d2c956ee290aa18a5d354b2a9ed2026d8f11
dolorina <marinadolokov@gmx.de>
2022-01-23 11:33:17 +0100

    completed train_model.py

--- starter/train_model.py
+++ starter/train_model.py
@@ -1,10 +1,14 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
+import pandas as pd
+import sys
+sys.path.insert(1, './ml') # change sys.path for function import
+from data import process_data
+from model import train_model, inference, compute_model_metrics
 
-# Add the necessary imports for the starter code.
-
-# Add code to load in the data.
+# load in the data
+data = pd.read_csv('../data/census_preprocessed.csv')
 
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
 train, test = train_test_split(data, test_size=0.20)
@@ -19,10 +23,15 @@ cat_features = [
     "sex",
     "native-country",
 ]
+
+# processing train and test data with process_data function
 X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+    train, categorical_features=cat_features, label="salary", training=True)
 
-# Proces the test data with the process_data function.
+X_test, y_test, encoder_test, lb_test = process_data(
+    test, categorical_features=cat_features, label="salary", training=True)
 
-# Train and save a model.
+# training and saving the model 
+model = train_model(X_train, y_train)
+preds = inference(model, X_test)
+precision, recall, fbeta = compute_model_metrics(y_test, preds)
