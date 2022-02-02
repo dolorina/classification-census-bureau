97a28b281f83873250747835bbcbd8146346b647
dolorina <marinadolokov@gmx.de>
2022-02-02 13:07:52 +0100

    trouble shooting in githubactions

--- ml/__init__.py
+++ ml/__init__.py
@@ -1,2 +1,2 @@
 import sys
-sys.path.append('/home/runner/work/classification-census-bureau/classification-census-bureau/ml/')
+sys.path.append('/home/runner/work/classification-census-bureau/classification-census-bureau/')
