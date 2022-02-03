d93481f29e2e3fb9f2cb8ab0dd7cd1a5095c7623
dolorina <marinadolokov@gmx.de>
2022-02-03 15:49:39 +0100

    new api tests

---
+++ tests/test_main.py
@@ -0,0 +1,58 @@
+'''
+Unit tests for API App
+
+Author: Marina Dolokov
+Date: February 2022 
+
+'''
+from fastapi.testclient import TestClient
+
+# Import our app from main.py.
+from main import app
+
+# Instantiate the testing client with our app.
+client = TestClient(app)
+
+# Write tests using the same syntax as with the requests module.
+def test_get():
+    r = client.get("/")
+    assert r.status_code == 200
+    assert r.json() == {"message": "Welcome to the census classification API"}
+
+def test_post():
+    r = client.post("/items/")
+    assert r.status_code == 422
+
+def test_post_prediction():
+    r = client.post("/items/", 
+                    json = {"age": 42,
+                            "workclass": "Self-emp-not-inc",
+                            "education": "HS-grad",
+                            "marital_status": "Married-civ-spouse",
+                            "occupation": "Farming-fishing",
+                            "relationship": "Husband",
+                            "race": "Asian-Pac-Islander",
+                            "sex": "Male",
+                            "capital_gain": 0,
+                            "capital_loss": 0,
+                            "hours_per_week": 40,
+                            "native_country": "Cambodia"
+                            }
+                    )
+    assert r.json() == {"Predicted salary": ">50k"}
+    r = client.post("/items/", 
+                    json = {"age": 50,
+                            "workclass": "Self-emp-not-inc",
+                            "education": "Bachelors",
+                            "marital_status": "Married-civ-spouse",
+                            "occupation": "Exec-managerial",
+                            "relationship": "Husband",
+                            "race": "White",
+                            "sex": "Male",
+                            "capital_gain": 0,
+                            "capital_loss": 0,
+                            "hours_per_week": 13,
+                            "native_country": "United-States"
+                            }
+                    )
+    assert r.json() == {"Predicted salary": "<=50k"}
---
+++ tests/test_model.py
@@ -0,0 +1,72 @@
+'''
+Script that contains unit test functions
+
+Author: Marina Dolokov
+Date: Februar 2022
+'''
+
+from data import process_data
+from model import compute_model_metrics, inference, train_model, metrics_on_fixed_features
+
+import pandas as pd
+import numpy as np 
+from sklearn.model_selection import train_test_split
+
+def load_data():
+    # load in the data
+    data = pd.read_csv('./data/census_preprocessed.csv')
+    train, test = train_test_split(data, test_size=0.20)
+    cat_feat = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    return train, test, cat_feat
+
+def test_process_data():
+    train, test, cat_feat = load_data()
+    X, y, _, _ = process_data(train, categorical_features=cat_feat, label="salary", training=True)
+    assert(X.shape[0]==y.shape[0])
+    assert(type(X) == type(y) == np.ndarray)
+
+
+def test_inference():
+    train, test, cat_feat = load_data()
+    X_train, y_train, encoder, lb = process_data(train, categorical_features=cat_feat, label="salary", training=True)
+    X_test, y_test, _, _ = process_data(test, categorical_features=cat_feat, label="salary", training=False, encoder=encoder, lb=lb)
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)
+    assert(preds.all()==model.predict(X_test).all())
+    assert(preds.shape==y_test.shape)
+    assert(isinstance(preds, np.ndarray))
+
+
+def test_compute_model_metrics():
+    train, test, cat_feat = load_data()
+    X_train, y_train, encoder, lb = process_data(train, categorical_features=cat_feat, label="salary", training=True)
+    X_test, y_test, _, _ = process_data(test, categorical_features=cat_feat, label="salary", training=False, encoder=encoder, lb=lb)
+    model = train_model(X_train, y_train)
+    preds = inference(model, X_test)
+    acc, prec, rec, f1 = compute_model_metrics(y_test, preds)
+    assert(isinstance(acc, float))
+    assert(isinstance(prec, float))
+    assert(isinstance(rec, float))
+    assert(isinstance(f1, float))
+    # assert(prec >= 0.5)
+    # assert(rec >= 0.5)
+    # assert(rec >= 0.5)
+    # assert(acc >= 0.5)
+
+
+# def test_metrics_on_fixed_features():
+#     train, test, cat_feat = load_data()
+#     X_train, y_train, encoder, lb = process_data(train, categorical_features=cat_feat, label="salary", training=True)
+#     model = train_model(X_train, y_train)
+#     metrics_on_education = metrics_on_fixed_features(model, test, cat_feat, encoder, lb, feature="education")
+#     assert(type(metrics_on_education)=="pandas.core.frame.DataFrame")
+    
