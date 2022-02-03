6d9af6391e6c998ead3692be36c972b345ffedef
dolorina <marinadolokov@gmx.de>
2022-02-03 18:20:07 +0100

    connect heroku to dvc

--- ml/main.py
+++ ml/main.py
@@ -11,6 +11,14 @@ import pandas as pd
 import numpy as np
 import pickle
 
+import os
+
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 model = pickle.load(open("./ml/mlp_classifier.sav", "rb"))
 encoder = pickle.load(open("./ml/mlp_encoder.sav", "rb"))
 
