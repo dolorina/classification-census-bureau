587fc47a1c62f05aea4a384774c2226191d333a7
dolorina <marinadolokov@gmx.de>
2022-02-05 20:24:44 +0100

    with dvc setup

--- main.py
+++ main.py
@@ -13,11 +13,11 @@ import pickle
 
 import os
 
-# if "DYNO" in os.environ and os.path.isdir(".dvc"):
-#     os.system("dvc config core.no_scm true")
-#     if os.system("dvc pull") != 0:
-#         exit("dvc pull failed")
-#     os.system("rm -r .dvc .apt/usr/lib/dvc")
+if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    os.system("dvc config core.no_scm true")
+    if os.system("dvc pull") != 0:
+        exit("dvc pull failed")
+    os.system("rm -r .dvc .apt/usr/lib/dvc")
 
 try:
     model = pickle.load(open("./ml/mlp_classifier.sav", "rb"))
