8662a1e0ca89cd43e7f1e2f6b52f61eff630fda4
dolorina <marinadolokov@gmx.de>
2022-02-05 17:23:48 +0100

    added path

--- tests/test_main.py
+++ tests/test_main.py
@@ -15,7 +15,7 @@ client = TestClient(app)
 
 # Write tests using the same syntax as with the requests module.
 def test_get():
-    r = client.get("/")
+    r = client.get("/items/")
     assert r.status_code == 200
     assert r.json() == {"message": "Welcome to the census classification API"}
 
