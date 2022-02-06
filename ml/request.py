6114616e669c6ac44fa1c4a6bffef85007b618ec
dolorina <marinadolokov@gmx.de>
2022-02-06 10:03:38 +0100

    live post with request

--- ml/request.py
+++ ml/request.py
@@ -1,22 +1,21 @@
 import requests
-# import json
+import json
 
-response = requests.post('https://census-bureau-app.herokuapp.com/')
+data = {"age": 42,
+        "workclass": "Self-emp-not-inc",
+        "education": "HS-grad",
+        "marital_status": "Married-civ-spouse",
+        "occupation": "Farming-fishing",
+        "relationship": "Husband",
+        "race": "Asian-Pac-Islander",
+        "sex": "Male",
+        "capital_gain": 0,
+        "capital_loss": 0,
+        "hours_per_week": 40,
+        "native_country": "Cambodia"
+        }
+# Making a POST request
+response = requests.post('https://census-bureau-app.herokuapp.com/', auth=('usr', 'pass'), data=json.dumps(data))
 
-# data = {"age": 42,
-#         "workclass": "Self-emp-not-inc",
-#         "education": "HS-grad",
-#         "marital_status": "Married-civ-spouse",
-#         "occupation": "Farming-fishing",
-#         "relationship": "Husband",
-#         "race": "Asian-Pac-Islander",
-#         "sex": "Male",
-#         "capital_gain": 0,
-#         "capital_loss": 0,
-#         "hours_per_week": 40,
-#         "native_country": "Cambodia"
-#         }
-# response = requests.post('http://127.0.0.1:8000/items/', auth=('usr', 'pass'), data=json.dumps(data))
-
-print(response.status_code)
-print(response.json())
+print("Check status code for response received:", response.status_code)
+print("Print content of request:", response.json())
